1ec63a7321dfff374c1c7f84f224e5a04beec157
Gilberto Silva <gilbertozn527@gmail.com>
2026-01-22 00:41:34 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@providers': path.resolve(__dirname, 'src/providers'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@routes': path.resolve(__dirname, 'src/routes'),
+    }
+  }
+})
