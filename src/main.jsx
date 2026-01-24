b62bffb989a598221aaec305af5dc9f5d6a3efd0
Gilberto Silva <gilbertozn527@gmail.com>
2026-01-24 20:35:06 -0300

    fix: corrigi pequenas coisas

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './global.css'
 import App from './App.jsx'
 
+
+// Configura o Leaflet
+import 'leaflet/dist/leaflet.css' 
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- src/pages/green-roof/create/CreateGreenRoof.jsx
+++ src/pages/green-roof/create/CreateGreenRoof.jsx
@@ -1,5 +1,9 @@
 import styles from './CreateGreenRoof.module.css'
 
 export default function CreateGreenRoof() {
-    
+    return (
+        <div>
+            Create green roof
+        </div>
+    )
 }
