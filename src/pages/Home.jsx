80e61028d4b7b8f26d03c71bcd697afd59ed7901
Gilberto Silva <gilbertozn527@gmail.com>
2026-01-24 20:33:08 -0300

    feature: adiciona pagina home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,25 @@
 import styles from './Home.module.css'
+import { useRef, useEffect } from 'react'
+
+import Map from '@components/map/Map'
+import ActionBar from '@components/action-bar/ActionBar'
 
 export default function Home() {
-    
+
+  const markers = [
+    {
+      name: 'Telhado verde SOFTEX',
+      type: 'Extensivo',
+      area: 734,
+      latitude: -8.061616277946458, 
+      longitude: -34.87220432682779,
+    }
+  ];
+
+  return (
+    <>
+      <ActionBar />
+      <Map markers={markers}/>
+    </>
+  )
 }
