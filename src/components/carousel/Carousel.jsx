cbcab703bd10ceb5792c8c60d60c46ac3daf3058
Gilberto Silva <gilbertozn527@gmail.com>
2026-02-01 20:29:07 -0300

    feature: adiciona o componente Carousel.jsx

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import styles from './Carousel.module.css';
+
+const Carousel = ({ images }) => {
+  const [indiceAtual, setIndiceAtual] = useState(0);
+
+  const proximaImagem = () => {
+    if (indiceAtual === images.length - 1) {
+      setIndiceAtual(0);
+    } else {
+      setIndiceAtual(indiceAtual + 1);
+    }
+  };
+
+  const imagemAnterior = () => {
+    if (indiceAtual === 0) {
+      setIndiceAtual(images.length - 1);
+    } else {
+      setIndiceAtual(indiceAtual - 1);
+    }
+  };
+
+  if (!images || images.length === 0)
+    return <div>Sem imagens para exibir</div>;
+
+  return (
+    <div className={styles["carousel-container"]}>
+      <button onClick={imagemAnterior} className={`${styles["btn-control"]} ${styles["left"]}`}>&#10094;</button>
+      <img
+        src={images[indiceAtual]}
+        alt={`Slide ${indiceAtual}`}
+        className={styles["carousel-image"]}
+      />
+      <button onClick={proximaImagem} className={`${styles["btn-control"]} ${styles["right"]}`}>&#10095;</button>
+    </div>
+  );
+};
+
+export default Carousel;
