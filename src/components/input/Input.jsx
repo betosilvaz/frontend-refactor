cecd35228369c75a6c0ca651b7a0c11ef3b0b870
Gilberto Silva <gilbertozn527@gmail.com>
2026-02-01 20:34:37 -0300

    fix: altera formatação de código

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,5 +1,5 @@
 import styles from './Input.module.css'
 
 export default function Input(props) {
-    return <input {...props} className={styles.input}></input>
+    return <input {...props} className={styles.input}/>
 }
