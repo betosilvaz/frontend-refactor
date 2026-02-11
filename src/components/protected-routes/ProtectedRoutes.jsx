8d3538cda5dfadcd7f0e1b2ee6da4e44d1e336d5
Gilberto Silva <gilbertozn527@gmail.com>
2026-02-10 21:35:41 -0300

    fix: corrige a logica de protação de rotas

--- src/components/protected-routes/ProtectedRoutes.jsx
+++ src/components/protected-routes/ProtectedRoutes.jsx
@@ -1,10 +1,7 @@
 import { Outlet, Navigate } from "react-router"
 import { jwtDecode } from "jwt-decode";
 
-import { useAuth } from '@providers/AuthProvider'
-
 export default function ProtectedRoutes({ allowedRoles }) {
-    const { isAuthenticated } = useAuth();
 
     let jwt = localStorage.getItem("jwt");
     if (!jwt) return <Navigate to="/login" replace/>
@@ -12,7 +9,7 @@ export default function ProtectedRoutes({ allowedRoles }) {
     let decoded = jwtDecode(jwt);
     let currentTime = Date.now() / 1000;
 
-    if(isAuthenticated && ( decoded.exp > currentTime )) {
+    if(( decoded.exp > currentTime )) {
         if (!allowedRoles || allowedRoles.length === 0) return <Outlet/>
         for(let role of decoded.roles)
             if(allowedRoles.includes(role)) return <Outlet/>
