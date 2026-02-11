a6d8fd826882378e7b8f6d4d9334b3b5c1dac3d6
Gilberto Silva <gilbertozn527@gmail.com>
2026-02-10 21:37:39 -0300

    fix: corrige defeito nos cards da pagina de pesquisa

--- src/pages/search/green-roof/card/Card.jsx
+++ src/pages/search/green-roof/card/Card.jsx
@@ -2,17 +2,19 @@ import styles from './Card.module.css'
 
 import { Link } from 'react-router'
 
+import { API_URL } from '@config/api/api.js'
+
 export default function Card({ data }) {
   return (
     <div className={styles.card}>
-      <img src={data.image} />
+      <img src={`${API_URL}/${data.images[0]}`} />
       <div className={styles.cardInfo}>
-        <h2>{data.name}</h2>
         <span className={styles.tag}>{data.type}</span>
+        <h2>{data.name}</h2>
         <span className={styles.address}>{data.address}</span>
         <p className={styles.description}>{data.description}</p>
       </div>
-      <Link className={styles.button} to={data.url}>Detalhes</Link>
+      <Link className={styles.button} to={`/green-roof/${data?.id}`}>Detalhes</Link>
     </div>
   )
 }
