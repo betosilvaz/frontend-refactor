f5956452d3ae954bcee95d60fa06d3bd65846671
Gilberto Silva <gilbertozn527@gmail.com>
2026-03-04 16:14:42 -0300

    feature: adiciona camadas diferentes do mapa

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,22 +1,44 @@
 import styles from './Map.module.css'
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMap, LayersControl } from 'react-leaflet'
 import MarkerClusterGroup from 'react-leaflet-cluster'
-import { Link } from 'react-router'
+import { useEffect } from 'react'
 
 import MapPopup from '@components/map-popup/MapPopup'
+const { BaseLayer } = LayersControl;
 
-export default function Map({ markers = []}) {
-  const circles = {
-    "Extensivo": '🟢',
-    "Semi-Intensivo": '🟡',
-    "Intensivo": '🔴'
-  }
+export default function Map({ markers = [], initialPosition = [-8.058211417035023, -34.871517645983225] }) {
 
   return (
-    <MapContainer center={[-8.058211417035023, -34.871517645983225]} zoom={13} className={styles.map}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution="&copy; OpenStreetMap"/>
-      <MarkerClusterGroup chunkLoading>
+    <MapContainer center={initialPosition} zoom={13} className={styles.map}>
+      <ChangeView center={initialPosition} zoom={16} />
+
+      <LayersControl position="topright">
+        <BaseLayer checked name="Mapa Colorido (Voyager)">
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+            keepBuffer={8}
+          />
+        </BaseLayer>
+
+        <BaseLayer name="Mapa Escuro">
+          <TileLayer
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://carto.com/">CARTO</a>'
+            keepBuffer={8}
+          />
+        </BaseLayer>
+
+        <BaseLayer name="Mapa Claro">
+          <TileLayer 
+            url="https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            keepBuffer={8}
+          />
+        </BaseLayer>
+      </LayersControl>
+      <MarkerClusterGroup chunkLoading showCoverageOnHover={false} maxClusterRadius={50}>
         {markers.map((marker) => (
           <Marker position={[marker.latitude, marker.longitude]}>
             <Popup>
@@ -27,4 +49,18 @@ export default function Map({ markers = []}) {
       </MarkerClusterGroup>
     </MapContainer>
   );
+}
+
+function ChangeView({ center, zoom }) {
+  const map = useMap();
+  useEffect(() => {
+    if (center) {
+      map.flyTo(center, zoom, {
+        duration: 3,
+        easeLinearity: 0.1
+      });
+    }
+  }, [center, zoom, map]);
+
+  return null;
 }
