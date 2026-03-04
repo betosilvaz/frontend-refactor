e4b1c9b0f994847cd443da12a78ea93446f16bec
Gilberto Silva <gilbertozn527@gmail.com>
2026-03-04 16:15:47 -0300

    feature: adiciona barra de pesquisa na pagina principal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,14 @@ import { useRef, useEffect, useState } from 'react'
 
 import Map from '@components/map/Map'
 import ActionBar from '@components/action-bar/ActionBar'
+import SearchIcon from '@components/icons/SearchIcon'
 
 import  { API_URL } from '@config/api/api.js'
+import toast from 'react-hot-toast'
 
 export default function Home() {
   const [markers, setMarkers] = useState([]);
+  const [initialPosition, setInitialPosition] = useState([-8.058211417035023, -34.871517645983225]); // São Paulo como posição inicial
 
   useEffect(() => {
     async function getGreenRoofs() {
@@ -32,10 +35,53 @@ export default function Home() {
     getGreenRoofs();
   }, []);
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const query = event.target.query.value;
+    try {
+      let url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}`
+      let response = await fetch(url, {
+        headers: {
+          'User-Agent': 'GreenRoofApp/1.0'
+        }
+      });
+
+      if (!response.ok) {
+        return toast.error("Erro ao se comunicar com o serviço de geocodificação");
+      }
+
+      let data = await response.json();
+
+      if (data.length === 0) {
+        return toast.error("Nenhum resultado encontrado para o endereço informado");
+      }
+
+      const { lat, lon, display_name } = data[0];
+      
+      setInitialPosition([parseFloat(lat), parseFloat(lon)]);
+      toast.success(`Localização atualizada para: ${display_name}`);
+    } catch (err) {
+      toast.error("Um erro inesperado aconteceu ao buscar o endereço");
+    }
+  }
+
+
   return (
     <>
       <ActionBar />
-      <Map markers={markers}/>
+      <SearchBar onSubmit={handleSubmit}/>
+      <Map markers={markers} initialPosition={initialPosition}/>
     </>
+  )
+}
+
+function SearchBar({ onSubmit }) {
+  return (
+    <form className={styles.searchBar}onSubmit={onSubmit} >
+      <input type="text" name="query" placeholder="Pesquise por endereços"/>
+      <button type="submit">
+        <SearchIcon />
+      </button>
+    </form>
   )
 }
