2227aa1845ad6271fc074a00327b236d15c1f5fb
Gilberto <gilbertozn527@gmail.com>
2026-03-07 00:36:16 -0300

    chore: organiza o componente ActionBar

--- src/components/action-bar/ActionBar.jsx
+++ src/components/action-bar/ActionBar.jsx
@@ -1,26 +1,24 @@
 import styles from './ActionBar.module.css'
+
 import { createPortal } from 'react-dom'
 import { Link } from 'react-router';
 import { useState } from 'react';
+import { AnimatePresence, motion } from 'motion/react';
 
 import SideBar from '@components/side-bar/SideBar'
 import SearchIcon from '@components/icons/SearchIcon'
 import HomeIcon from '@components/icons/HomeIcon'
 import MenuIcon from '@components/icons/MenuIcon'
-import { AnimatePresence, motion } from 'motion/react';
 
 export default function ActionBar() {
   const [sidebar, setSidebar] = useState(false);
 
-  function closeSideBar() {
-    setSidebar(false);
-  }
+  const toggleSideBar = () => setSidebar(prev => !prev);
 
   return createPortal((
     <>
       <AnimatePresence>
-        {sidebar &&  (
-          /* 1. O OVERLAY ESCURO: Fica fixo e faz apenas fade in/out */
+        {sidebar && (
           <motion.div 
             key="overlay"
             initial={{ opacity: 0 }}
@@ -29,29 +27,28 @@ export default function ActionBar() {
             style={{
               position: 'fixed',
               top: 0, left: 0, right: 0, bottom: 0,
-              backgroundColor: 'rgba(0, 0, 0, 0.5)', // Cor do fundo escuro
+              backgroundColor: 'rgba(0, 0, 0, 0.5)',
               zIndex: 9999
             }}
-            onClick={closeSideBar} // Clicar no fundo escuro fecha o menu
+            onClick={toggleSideBar}
           >
             <motion.div 
-              key="sidebar-menu"
+              key="sidebar"
               initial={{x: "100%"}} 
               animate={{x: 0}}
               exit={{x: "100%"}} 
               transition={{ type: 'spring', stiffness: 300, damping: 30 }}
               style={{
-                position: 'absolute', // Absolute em relação ao overlay
+                position: 'absolute',
                 top: 0,
                 right: 0, 
                 height: '100vh',
-                // Remova o width abaixo se a largura já estiver definida no seu SideBar.module.css
                 width: '300px', 
-                zIndex: 100000000 // Certifique-se de que o z-index do menu seja maior que o do overlay
+                zIndex: 100000000
               }}
               onClick={(e) => e.stopPropagation()} // Evita que clicar no menu feche ele
             >
-              <SideBar onClose={closeSideBar}/>
+              <SideBar onClose={toggleSideBar}/>
             </motion.div>
           </motion.div>
         )}
@@ -60,9 +57,7 @@ export default function ActionBar() {
       <nav className={styles.actionBar}>
         <Link to="/search"><SearchIcon/></Link>
         <Link to="/"><HomeIcon/></Link>
-        <button onClick={() => setSidebar(prev => !prev)} className={styles.menuButton}>
-          <MenuIcon/>
-        </button>
+        <button className={styles.menuButton} onClick={toggleSideBar}><MenuIcon/></button>
       </nav>
     </>
   ), document.body);
