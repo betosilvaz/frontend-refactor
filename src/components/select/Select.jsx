27cec22b37b0f8fa740880ffcfa3d65053a05dd9
Gilberto <gilbertozn527@gmail.com>
2026-03-06 23:31:16 -0300

    fix: corrige erro na lógica do componente Select que não possuia prop para especificar o atributo name do input

--- src/components/select/Select.jsx
+++ src/components/select/Select.jsx
@@ -1,8 +1,8 @@
 import styles from './Select.module.css'
 
-export default function Select({ value, options, onSelect }) {
+export default function Select({ name, value, options, onSelect }) {
   return (
-    <select name="type" className={styles.select} value={value ?? ""} onChange={onSelect}>
+    <select name={name} className={styles.select} value={value ?? ""} onChange={onSelect}>
       <option value="" disabled>Selecione</option>
       {options.map((item) =>
         <option value={item.value}>{item.name}</option>
