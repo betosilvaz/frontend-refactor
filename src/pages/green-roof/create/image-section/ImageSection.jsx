d69602e51356e11448ad03d5c095df182b711a3c
Gilberto <gilbertozn527@gmail.com>
2026-03-10 13:40:23 -0300

    refactor: refatore ImageSection

--- src/pages/green-roof/create/image-section/ImageSection.jsx
+++ src/pages/green-roof/create/image-section/ImageSection.jsx
@@ -1,11 +1,6 @@
 import ImageUploader from "@components/image-uploader/ImageUploader";
 
-import styles from "./ImageSection.module.css";
-
 export default function ImageSection({ images, addImage, removeImage }) {
-  return (
-    <form className={styles.imagesForm}>
-      <ImageUploader images={images ?? []} addImage={addImage} removeImage={removeImage} />
-    </form>
-  );
+  const defaultValue = {originals: [], toAdd: []};
+  return <ImageUploader images={images ?? defaultValue} addImage={addImage} removeImage={removeImage} />;
 }
