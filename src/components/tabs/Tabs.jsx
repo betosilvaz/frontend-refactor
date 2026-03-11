33c6bb6dfd37238e2c9995aadcf52c86a36b22e1
Gilberto Silva <gilbertozn527@gmail.com>
2026-03-11 16:13:46 -0300

    refactor: reestrutura o componente Tabs para utilizar composição e com isso facilitar o seu uso e flexibilidade

--- src/components/tabs/Tabs.jsx
+++ src/components/tabs/Tabs.jsx
@@ -1,15 +1,45 @@
 import styles from "./Tabs.module.css";
 
-export default function Tabs({ changeTab, options, actual }) {
+import { createContext, useContext, useState } from "react";
+
+const TabsContext = createContext();
+
+function Tabs({ children, defaultValue }) {
+  const [active, setActive] = useState(defaultValue);
+
   return (
-    <div className={styles.tabs}>
-      {options.map((e, index) => {
-        return (
-          <span className={actual === index + 1 ? styles.selected : ""} onClick={() => changeTab(index + 1)}>
-            {e}
-          </span>
-        );
-      })}
-    </div>
+    <TabsContext.Provider value={{ active, setActive }}>
+      {children}
+    </TabsContext.Provider>
   );
 }
+
+function List({ children }) {
+  return <div className={styles.tabs}>{children}</div>;
+}
+
+function Content({ children, value }) {
+  const { active } = useContext(TabsContext);
+
+  if (active !== value) return null;
+
+  return <div className={styles.forms}>{children}</div>;
+}
+
+function Trigger({ children, value }) {
+  const { active, setActive } = useContext(TabsContext);
+
+  const selected = active === value;
+
+  return (
+    <button onClick={() => setActive(value)} className={selected ? styles.selected : ""}>
+      {children}
+    </button>
+  );
+}
+
+Tabs.List = List;
+Tabs.Trigger = Trigger;
+Tabs.Content = Content;
+
+export default Tabs;
