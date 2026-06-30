7f5c5445fcb50d66a378d57ed2e20d7904a6f896
Gilberto Silva <gilbertozn527@gmail.com>
2026-06-30 15:48:06 -0300

    refactor: limpa lógica do fetchThis e remove console.logs desnecessários

--- src/utils/fetchThis.js
+++ src/utils/fetchThis.js
@@ -1,5 +1,13 @@
 import { API_URL } from "@config/api/api.js";
 
+const options = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include'
+};
+
+const endpoint = `${API_URL}/api/auth/refresh`;
+
 /**
  * fetchThises data from a REST API endpoint with 
  * @param {String} url 
@@ -9,34 +17,24 @@ import { API_URL } from "@config/api/api.js";
 async function fetchThis(url, options = {}) {
     let response = await fetch(url, options);
     if (response.status === 401) {
-        console.log("Access token expirado. Tentando renovar...");
         try {
-            const refreshResponse = await fetch(`${API_URL}/api/auth/refresh`, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                credentials: 'include' // envia cookies (refresh token) automaticamente
-            });
-
-            console.log("Resposta do refresh token:", refreshResponse);
+            const refreshResponse = await fetch(endpoint, options);
 
             if (refreshResponse.ok) {
                 const data = await refreshResponse.json();
-                const newAccessToken = data.jwt; // pega o access token renovado
+                const newAccessToken = data.jwt;
                 localStorage.setItem("jwt", newAccessToken);
-                options.headers = {
-                    ...options.headers,
-                    'Authorization': `Bearer ${newAccessToken}`
-                };
+                options.headers = { ...options.headers, 'Authorization': `Bearer ${newAccessToken}` };
                 response = await fetch(url, options);
-            } else {
-                console.error("Falha ao renovar o token. O usuário precisa fazer login novamente.");
-                localStorage.removeItem('jwt'); // remove o access token antigo
-                // window.location.href = '/login';
+            } else {;
+                localStorage.removeItem('jwt');
+                location.href = '/login';
             }
         } catch (error) {
-            console.error("Erro na comunicação com o endpoint de refresh token:", error);
+            location.href = '/login';
         }
     }
     return response;
 }
+
 export default fetchThis;
