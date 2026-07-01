11117e9d845c909570d7be841c916c12711b8198
Gilberto Silva <gilbertozn527@gmail.com>
2026-07-01 14:59:10 -0300

    chore: atualiza a config do eslit

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,13 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'dev-dist']),
+  {
+    files: ['vite.config.js', 'eslint.config.js'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
   {
     files: ['**/*.{js,jsx}'],
     extends: [
